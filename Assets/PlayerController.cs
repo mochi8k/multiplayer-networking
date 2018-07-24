3c49055346a5920a16a12c4655166bdda0a4e9be
mochi8k <y.hkh.185@gmail.com>
2018-07-25 08:51:49 +0900

    10. Shooting (Single Player)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,10 @@
 using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     void Update() {
 
         if (!isLocalPlayer) {
@@ -13,10 +17,26 @@ public class PlayerController : NetworkBehaviour {
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fire();
+        }
+    }
+
+    void Fire() {
+        // BulletプレハブからBulletを生成する
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // 弾の速度を増加させる
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // 2秒後に弾を破壊する
+        Destroy(bullet, 2.0f);
     }
 
     public override void OnStartLocalPlayer() {
         GetComponent<MeshRenderer>().material.color = Color.blue;
     }
+
 
 }
